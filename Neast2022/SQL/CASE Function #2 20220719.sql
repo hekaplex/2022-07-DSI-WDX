16a086bcb507b478c1a1a5f15a5bfda9d40e6932
Neast2022 <neaston1@gmail.com>
2022-07-19 13:02:22 -0500

    Who are the vendors with zipcodes starting with 7?

---
+++ Neast2022/SQL/CASE Function #2 20220719.sql
@@ -0,0 +1,10 @@
+/*Who are the vendors that are local to zip codes starting with a 7 */
+SELECT *, 
+CASE
+WHEN VendorZipCode like '7%' THEN 'Local'
+ELSE 'Not Local'
+END
+AS VendorLocation
+FROM Vendors
+ORDER BY 
+VEndorZipCode
