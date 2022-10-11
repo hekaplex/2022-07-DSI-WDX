6ca00d8e1c22ecf269a90d84200643358881837b
Neast2022 <109098192+Neast2022@users.noreply.github.com>
2022-10-11 12:46:30 -0500

    Add files via upload

---
+++ Neast2022/Python/import locale.py
@@ -0,0 +1,16 @@
+import locale
+
+# set the locale for use in currency formatting
+locale.setlocale(locale.LC_ALL, 'en_US')
+
+# display a welcome message
+print (" Welcome to the future Value Calculator")
+print()
+
+choice = "y":
+# get input from the user
+monthly_investment = float (input("Enter yearly intrest rate:\t\t"))
+
+# convert yearly values to monthly values
+monthly_interest_rate / 12/ 100
+months = years * 12
